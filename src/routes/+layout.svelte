a2893789e9838e59ab30474ad1b8c32f81290396
TGleason-Kaiser <togleasonkaiser@gmail.com>
2025-03-14 18:12:21 -0400

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../app.css';
+	import { DarkMode, Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import {
+		HomeSolid,
+		QuestionCircleSolid,
+		ShoppingBagSolid,
+		AddressBookSolid
+	} from 'flowbite-svelte-icons';
+	let { children } = $props();
+</script>
+
+<Navbar>
+	<NavHamburger />
+	<NavUl>
+		<NavLi><DarkMode /></NavLi>
+		<NavLi href="/"><HomeSolid /></NavLi>
+		<NavLi href="/about"><QuestionCircleSolid /></NavLi>
+		<NavLi href="/pricing"><ShoppingBagSolid /></NavLi>
+		<NavLi href="/contact"><AddressBookSolid /></NavLi>
+	</NavUl>
+</Navbar>
+
+{@render children()}
